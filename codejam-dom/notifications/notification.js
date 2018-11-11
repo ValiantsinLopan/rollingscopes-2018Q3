29eb5af373bc829214d98a05967855f6c307ea83
Valentin Lopan <valiantsin_lopan@live.com>
2018-11-11 16:15:44 +0300

    feat: add Disable functionality

--- codejam-dom/notifications/notification.js
+++ codejam-dom/notifications/notification.js
@@ -1,15 +1,27 @@
 /* eslint-disable no-undef */
 const notificationModal = document.getElementById('notification-modal');
 const closeButton = document.getElementById('close');
+const disableCheckbox = document.getElementById('disable');
 
 window.onload = () => {
-  setTimeout(() => {
-    notificationModal.style.display = 'block';
-  }, 5000);
+  if (localStorage.getItem('showNotification') !== 'false') {
+    setTimeout(() => {
+      notificationModal.style.display = 'block';
+    }, 5000);
+  }
 };
 
 function closeNotifaication() {
   notificationModal.style.display = 'none';
 }
 
+function onCheckboxChange() {
+  if (disableCheckbox.checked) {
+    localStorage.setItem('showNotification', 'false');
+  } else {
+    localStorage.setItem('showNotification', 'true');
+  }
+}
+
 closeButton.addEventListener('click', closeNotifaication);
+disableCheckbox.addEventListener('change', onCheckboxChange);
