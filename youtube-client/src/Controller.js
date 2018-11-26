a52185c5072dc0c7937425ed0fc5bedf00bf45b5
Valentin Lopan <valiantsin_lopan@live.com>
2018-11-27 00:58:24 +0300

    feat: new logic of rendering results items

--- youtube-client/src/Controller.js
+++ youtube-client/src/Controller.js
@@ -1,5 +1,6 @@
 import Model from './Model';
 import View from './View';
+import Carousel from './components/Carousel';
 
 export default class Controller {
   constructor() {
@@ -12,15 +13,32 @@ export default class Controller {
   handleSeachInput(e) {
     const key = e.which || e.keyCode;
     if (key === 13) {
+      if (e.target.value === this.searchQuery) {
+        console.log('update!');
+        this.model.getResults(e.target.value,
+          (data) => {
+            this.view.results.renderResultsItems(data.items);
+            this.carousel.update();
+            this.searchResults.push(...data.items);
+          });
+        return;
+      }
+      this.searchQuery = e.target.value;
       this.view.results.clear();
       this.view.results.render();
       this.model.getResults(e.target.value,
         (data) => {
-          this.view.results.initCarousel(data.items);
+          this.view.results.renderResultsItems(data.items);
+          this.carousel = new Carousel();
+          this.carousel.init();
           this.searchResults.push(...data.items);
         });
-      document.getElementById('items').addEventListener('animationend', () => console.log('animation end'));
-      console.log(this.searchResults);
+      window.addEventListener('resize',
+        () => {
+          console.log('resize');
+          this.carousel = new Carousel();
+          this.carousel.init();
+        });
     }
   }
 
