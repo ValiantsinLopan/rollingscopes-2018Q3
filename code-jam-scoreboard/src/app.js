1c0c38abd7281dfcdc50c90ab4af02fb3dad0e44
Valentin Lopan <valiantsin_lopan@live.com>
2018-12-09 22:55:04 +0300

    feat: add base functional

--- code-jam-scoreboard/src/app.js
+++ code-jam-scoreboard/src/app.js
@@ -1,5 +1,11 @@
-fetch('../data/sessions.json')
-  .then(res => res.json())
-  .then((data) => {
-    console.log('data:', data);
-  });
+import sessions from '../data/sessions';
+import Table from './Table';
+
+window.onload = () => new Table(sessions[1]);
+
+document.querySelector('#switch').addEventListener('click', (e) => {
+  document.querySelector('table').remove();
+  if (e.target.value === 'rsschool') {
+    return new Table(sessions[1]);
+  } return new Table(sessions[0]);
+});
