3e0a7819c46a5bc9952ad729111f780f09636401
Valentin Lopan <valiantsin_lopan@live.com>
2019-01-07 14:38:55 +0300

    feat: add base characters

--- game/src/components/GameField/GameField.js
+++ game/src/components/GameField/GameField.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import coridor from './images/coridor.jpg';
+import boo from './images/boo.png';
+import monster from './images/monster.png';
 
 class GameField extends Component {
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.gameField}>
-        
+        <Grid container className={classes.players}>
+          <Grid item xs={5}>
+            <img src={boo} alt="boo" className={classes.photo} />
+          </Grid>
+          <Grid item xs />
+          <Grid item xs={5}>
+            <img src={monster} alt="monster" className={classes.photo} />
+          </Grid>
+        </Grid>
+        <Button variant="contained" size="large" color="primary">
+          Select spell
+        </Button>
       </div>
     );
   }
@@ -22,6 +37,14 @@ const styles = {
     margin: '0',
     backgroundImage: `url(${coridor})`,
     backgroundPosition: 'bottom',
+    textAlign: 'center',
+  },
+  players: {
+    height: '70%',
+  },
+  photo: {
+    maxHeight: '100%',
+    margin: 'auto',
   },
 };
 
