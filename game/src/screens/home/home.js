0b72bf12948905c817732de6b6b1fc70f0f223d8
Valentin Lopan <valiantsin_lopan@live.com>
2019-01-08 03:17:50 +0300

    feat: update landing

--- game/src/screens/home/home.js
+++ game/src/screens/home/home.js
@@ -1,44 +1,68 @@
 import React, { Component } from 'react';
 import {
-  Heading, Hero, Subhead, Flex, ScrollDownIndicator, Image,
+  Heading, Hero, Subhead, Flex, ScrollDownIndicator, Image, Laptop, Contributor, Small,
 } from 'react-landing-page';
 import Button from '@material-ui/core/Button';
 import monsters from './images/monsters.png';
+import gameplay from './images/gameplay.png';
+import avatar from './images/avatar.jpg';
 import './home.css';
 
 class Home extends Component {
   render() {
     return (
-      <Hero
-        color="black"
-        bg="#95F0AD"
-      >
-
-        <Flex flexWrap="wrap" alignItems="center">
-          <Flex alignItems="flex-start" width={[1, 1 / 2]} p={3}>
-            <Image src={monsters} className="logo" />
-          </Flex>
-          <Flex width={[1, 1 / 2]} alignItems="center" flexDirection="column" p={3}>
-            <Heading className="header">Monsters School</Heading>
-            <Subhead textAlign="center">Amazing game for little monster hunters!</Subhead>
-            <Button
-              className="playButton"
-              variant="outlined"
-              color="primary"
-              size="large"
-              href="/game"
-              style={
-                  {
-                    margin: '10px 0 0 0',
+      <div>
+        <Hero
+          color="black"
+          bg="#95F0AD"
+        >
+          <Flex flexWrap="wrap" alignItems="center">
+            <Flex alignItems="flex-start" width={[1, 1 / 2]} p={3}>
+              <Image src={monsters} className="logo" />
+            </Flex>
+            <Flex width={[1, 1 / 2]} alignItems="center" flexDirection="column" p={3}>
+              <Heading className="header">Monsters School</Heading>
+              <Subhead textAlign="center">Amazing game for little monster hunters!</Subhead>
+              <Button
+                className="playButton"
+                variant="outlined"
+                color="primary"
+                size="large"
+                href="/game"
+                style={
+                    {
+                      margin: '10px 0 0 0',
+                    }
                   }
-                }
-            >
-                Play now!
-            </Button>
+              >
+                  Play now!
+              </Button>
+            </Flex>
           </Flex>
+          <ScrollDownIndicator />
+        </Hero>
+        <Hero
+          color="black"
+          bg="pink"
+        >
+          <Heading>Play and Study</Heading>
+          <Subhead fontSize={[2, 3]}>Solve tasks, fight with different monsters and become smarter</Subhead>
+          <Laptop mt={10} src={gameplay} />
+        </Hero>
+        <Heading textAlign="center">Made by</Heading>
+        <Flex justifyContent="space-around">
+          <Contributor
+            fullName="Valentin Lopan"
+            title="developer"
+            avatar={avatar}
+          >
+            <Flex>
+              <Button href="https://github.com/ValiantsinLopan">GitHub</Button>
+              <Button href="https://www.linkedin.com/in/valiantsin/">LinkedIn</Button>
+            </Flex>
+          </Contributor>
         </Flex>
-        <ScrollDownIndicator />
-      </Hero>
+      </div>
     );
   }
 }
