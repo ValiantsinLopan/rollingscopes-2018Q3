5d4a5a0f3cd9c92fe1f625eff5cfe4a3c1c58adb
Valentin Lopan <valiantsin_lopan@live.com>
2019-01-09 03:01:27 +0300

    refactor: landing styles via jsx

--- game/src/screens/home/home.js
+++ game/src/screens/home/home.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {
-  Heading, Hero, Subhead, Flex, ScrollDownIndicator, Image, Laptop, Contributor, Small,
+  Heading, Hero, Subhead, Flex, ScrollDownIndicator, Image, Laptop, Contributor,
 } from 'react-landing-page';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import monsters from './images/monsters.png';
 import gameplay from './images/gameplay.png';
 import avatar from './images/avatar.jpg';
-import './home.css';
 
 class Home extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Hero
@@ -19,13 +20,15 @@ class Home extends Component {
         >
           <Flex flexWrap="wrap" alignItems="center">
             <Flex alignItems="flex-start" width={[1, 1 / 2]} p={3}>
-              <Image src={monsters} className="logo" />
+              <Image src={monsters} className={classes.logo} />
             </Flex>
             <Flex width={[1, 1 / 2]} alignItems="center" flexDirection="column" p={3}>
-              <Heading className="header">Monsters School</Heading>
+              <Heading className={classes.header}>Monsters School</Heading>
               <Subhead textAlign="center">Amazing game for little monster hunters!</Subhead>
               <Link
                 to="/game"
+                textDecoration="none"
+                className={classes.link}
               >
                 <Button
                   className="playButton"
@@ -71,4 +74,17 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const styles = {
+  header: {
+    textAlign: 'center',
+    fontSize: '80px',
+  },
+  logo: {
+    maxHeight: '90vh',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+};
+
+export default withStyles(styles)(Home);
