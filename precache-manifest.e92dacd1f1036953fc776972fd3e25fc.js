79ac3f170bf4e3060b5cd962536edecfa9213f6c
Valentin Lopan <valiantsin_lopan@live.com>
2019-02-24 19:59:51 +0300

    Updates

---
+++ precache-manifest.e92dacd1f1036953fc776972fd3e25fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a7d663b60849a1a4ead",
+    "url": "/monstergame/rss-mentor-dashboard/static/js/runtime~main.6a7d663b.js"
+  },
+  {
+    "revision": "66547571db56cfb518f1",
+    "url": "/monstergame/rss-mentor-dashboard/static/js/main.66547571.chunk.js"
+  },
+  {
+    "revision": "721b7117b81964c42487",
+    "url": "/monstergame/rss-mentor-dashboard/static/js/2.721b7117.chunk.js"
+  },
+  {
+    "revision": "66547571db56cfb518f1",
+    "url": "/monstergame/rss-mentor-dashboard/static/css/main.3ace9ebe.chunk.css"
+  },
+  {
+    "revision": "b28d910d70f2633ce124095c9d136a81",
+    "url": "/monstergame/rss-mentor-dashboard/index.html"
+  }
+];
