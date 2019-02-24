dd557117e2c71da308a6026475eccf09f8c57bf5
Valentin Lopan <valiantsin_lopan@live.com>
2019-02-24 18:22:19 +0300

    feat: add locaStorage support

--- rss-mentor-dashboard/src/store/index.js
+++ rss-mentor-dashboard/src/store/index.js
@@ -3,8 +3,34 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { mentorsReducer } from './reducers/index';
 
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
 export const store = createStore(
-  mentorsReducer, composeWithDevTools(
+  mentorsReducer,
+  persistedState,
+  composeWithDevTools(
     applyMiddleware(thunk),
   ),
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
